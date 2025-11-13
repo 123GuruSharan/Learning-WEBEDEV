09cb6987d9afcdee0e4d0c67ef5b6550320b4cd0
123GuruSharan <sharanguru096@gmail.com>
2025-11-14 01:23:50 +0530

    Guru | Updated Day 2 loops practice files

--- GURU/DAY2_CONDITIONALS_LOOPS/Multiplication_Table.js
+++ GURU/DAY2_CONDITIONALS_LOOPS/Multiplication_Table.js
@@ -1,6 +1,6 @@
 //2️⃣ (Medium) Print the multiplication table of any given number using a loop.
 
-let num = Number(prompt("Enter a number: "));
+let num = Number(prompt("Enter a number to print table of: "));
 
 document.write("<h1>Here is the table of "+ num + "</h1>")
 for(let i = 1; i<= 10;i++)
--- GURU/DAY2_CONDITIONALS_LOOPS/PrimeNumbers.js
+++ GURU/DAY2_CONDITIONALS_LOOPS/PrimeNumbers.js
@@ -1,2 +1,30 @@
-//3️⃣ (Tough) Write a program that prints prime numbers between 1 and 100.
+//3️⃣ (Tough) Write a program that prints prime numbers between m and n,
 
+
+let m = Number(prompt("Enter the Starting range to find prime numbers: "));
+let n = Number(prompt("Enter the ending range to find prime numbers: "));
+
+document.write("<h1>Here is your Prime number program to print " + m + " to " + n + "</h1><br>");
+
+
+for(let i = m; i <= n ; i++)
+{
+    let isPrime = true
+    if(i <=1)
+    {
+        isPrime = false;
+    }
+
+    for(let j=2 ; j<i ; j++)
+    {
+        if(i%j == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime)
+    {
+        document.write(i + "<br>");
+    }
+}
