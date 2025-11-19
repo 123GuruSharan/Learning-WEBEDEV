379732bd5b8d88650f709041b7737ff1913d09c2
Vaibhav Kumar <vaibhav2016singh@gmail.com>
2025-11-19 11:14:39 +0530

    Added variables.js in DAY1

---
+++ VAIBHAV/DAY1/variables.js
@@ -0,0 +1,7 @@
+//Declare variables for your name, age, and favorite color. Print them in one line.
+
+let name="Vaibhav Kumar"
+let age=21;
+let favoriteColor="White";
+
+console.log(name,age,favoriteColor);
