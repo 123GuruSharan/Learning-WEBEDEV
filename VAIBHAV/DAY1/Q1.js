482122522f10dce8002230df57036d3b0f92f1e6
Vaibhav Kumar <vaibhav2016singh@gmail.com>
2025-11-19 13:44:28 +0530

    done question1

---
+++ VAIBHAV/DAY1/Q1.js
@@ -0,0 +1,15 @@
+//Store marks of 5 subjects and print total and average.
+
+let Marks=100;
+
+let marks1= 89;
+let marks2= 98;
+let marks3= 100;
+let marks4= 79;
+let marks5= 69;
+
+let TotalMarks=marks1+marks2+marks3+marks4+marks5;
+let average=(marks1+marks2+marks3+marks4+marks5)/5;
+
+console.log(TotalMarks);
+console.log(average);
